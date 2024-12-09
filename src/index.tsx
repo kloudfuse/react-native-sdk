3157673ba2215c0ba29c79f6f08366db957305e7
Andrew Nguyen <andrew@kloudfuse.com>
2024-12-08 21:45:20 -0800

    First version

--- src/index.tsx
+++ src/index.tsx
@@ -3,8 +3,18 @@ import {
   DatadogProviderConfiguration,
   SdkVerbosity,
 } from '@datadog/mobile-react-native';
+import { type ReactNode } from 'react';
 
-const KfProvider = ({ children, config }) => {
+type Props = {
+  children: ReactNode;
+  config: {
+    applicationId: string;
+    clientToken: string;
+    env: string;
+  };
+};
+
+const KfProvider = ({ children, config }: Props) => {
   const { applicationId, clientToken, env } = config;
   const ddConfig = new DatadogProviderConfiguration(
     clientToken,
@@ -15,19 +25,19 @@ const KfProvider = ({ children, config }) => {
     true // track errors
   );
 
-  config.site = 'US5';
+  ddConfig.site = 'US5';
   // Optional: Enable or disable native crash reports
-  config.nativeCrashReportEnabled = true;
+  ddConfig.nativeCrashReportEnabled = true;
   // Optional: Sample RUM sessions (in this example, 80% of session are sent to Datadog. Default is 100%).
-  config.sessionSamplingRate = 100;
+  ddConfig.sessionSamplingRate = 100;
   // Optional: Sample tracing integrations for network calls between your app and your backend (in this example, 80% of calls to your instrumented backend are linked from the RUM view to the APM view. Default is 20%)
   // You need to specify the hosts of your backends to enable tracing with these backends
-  config.resourceTracingSamplingRate = 100;
-  config.firstPartyHosts = ['example.com']; // matches 'example.com' and subdomains like 'api.example.com'
+  ddConfig.resourceTracingSamplingRate = 100;
+  ddConfig.firstPartyHosts = ['example.com']; // matches 'example.com' and subdomains like 'api.example.com'
   // Optional: set the reported service name (by default, it uses the package name or bundleIdentifier of your Android or iOS app respectively)
-  config.serviceName = 'com.example.reactnative';
+  ddConfig.serviceName = 'com.example.reactnative';
   // Optional: let the SDK print internal logs above or equal to the provided level. Default is undefined (meaning no logs)
-  config.verbosity = SdkVerbosity.WARN;
+  ddConfig.verbosity = SdkVerbosity.WARN;
 
   return <DatadogProvider configuration={ddConfig}>{children}</DatadogProvider>;
 };
